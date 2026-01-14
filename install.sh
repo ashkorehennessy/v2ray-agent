1deae69463be81501c1a2f1a1a02050360d2d9cc
ashkorehennessy <ashkorehennessy@gmail.com>
2026-01-15 01:13:34 +0800

    跳过端口和ip检测

--- install.sh
+++ install.sh
@@ -1370,99 +1370,11 @@ installWarp() {
 
 # 通过dns检查域名的IP
 checkDNSIP() {
-    local domain=$1
-    local dnsIP=
-    ipType=4
-    dnsIP=$(dig @1.1.1.1 +time=2 +short "${domain}" | grep -E "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
-    if [[ -z "${dnsIP}" ]]; then
-        dnsIP=$(dig @8.8.8.8 +time=2 +short "${domain}" | grep -E "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
-    fi
-    if echo "${dnsIP}" | grep -q "timed out" || [[ -z "${dnsIP}" ]]; then
-        echo
-        echoContent red " ---> 无法通过DNS获取域名 IPv4 地址"
-        echoContent green " ---> 尝试检查域名 IPv6 地址"
-        dnsIP=$(dig @2606:4700:4700::1111 +time=2 aaaa +short "${domain}")
-        ipType=6
-        if echo "${dnsIP}" | grep -q "network unreachable" || [[ -z "${dnsIP}" ]]; then
-            echoContent red " ---> 无法通过DNS获取域名IPv6地址，退出安装"
-            exit 0
-        fi
-    fi
-    local publicIP=
-
-    publicIP=$(getPublicIP "${ipType}")
-    if [[ "${publicIP}" != "${dnsIP}" ]]; then
-        echoContent red " ---> 域名解析IP与当前服务器IP不一致\n"
-        echoContent yellow " ---> 请检查域名解析是否生效以及正确"
-        echoContent green " ---> 当前VPS IP：${publicIP}"
-        echoContent green " ---> DNS解析 IP：${dnsIP}"
-        exit 0
-    else
-        echoContent green " ---> 域名IP校验通过"
-    fi
+    echo "跳过IP检测"
 }
 # 检查端口实际开放状态
 checkPortOpen() {
-    handleSingBox stop >/dev/null 2>&1
-    handleXray stop >/dev/null 2>&1
-
-    local port=$1
-    local domain=$2
-    local checkPortOpenResult=
-    allowPort "${port}"
-
-    if [[ -z "${btDomain}" ]]; then
-
-        handleNginx stop
-        # 初始化nginx配置
-        touch ${nginxConfigPath}checkPortOpen.conf
-        local listenIPv6PortConfig=
-
-        if [[ -n $(curl -s -6 -m 4 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | cut -d "=" -f 2) ]]; then
-            listenIPv6PortConfig="listen [::]:${port};"
-        fi
-        cat <<EOF >${nginxConfigPath}checkPortOpen.conf
-server {
-    listen ${port};
-    ${listenIPv6PortConfig}
-    server_name ${domain};
-    location /checkPort {
-        return 200 'fjkvymb6len';
-    }
-    location /ip {
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header REMOTE-HOST \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        default_type text/plain;
-        return 200 \$proxy_add_x_forwarded_for;
-    }
-}
-EOF
-        handleNginx start
-        # 检查域名+端口的开放
-        checkPortOpenResult=$(curl -s -m 10 "http://${domain}:${port}/checkPort")
-        localIP=$(curl -s -m 10 "http://${domain}:${port}/ip")
-        rm "${nginxConfigPath}checkPortOpen.conf"
-        handleNginx stop
-        if [[ "${checkPortOpenResult}" == "fjkvymb6len" ]]; then
-            echoContent green " ---> 检测到${port}端口已开放"
-        else
-            echoContent green " ---> 未检测到${port}端口开放，退出安装"
-            if echo "${checkPortOpenResult}" | grep -q "cloudflare"; then
-                echoContent yellow " ---> 请关闭云朵后等待三分钟重新尝试"
-            else
-                if [[ -z "${checkPortOpenResult}" ]]; then
-                    echoContent red " ---> 请检查是否有网页防火墙，比如Oracle等云服务商"
-                    echoContent red " ---> 检查是否自己安装过nginx并且有配置冲突，可以尝试DD纯净系统后重新尝试"
-                else
-                    echoContent red " ---> 错误日志：${checkPortOpenResult}，请将此错误日志通过issues提交反馈"
-                fi
-            fi
-            exit 0
-        fi
-        checkIP "${localIP}"
-    fi
+    echo "跳过端口检测"
 }
 
 # 初始化Nginx申请证书配置
